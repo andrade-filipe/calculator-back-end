ec46dd69adf601d994d490a00a1ae7292f92ae93
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2023-11-01 15:36:04 -0300

    Simple Tests added

---
+++ src/test/java/com/simple/calculator/domain/service/CalculatorServiceTest.java
@@ -0,0 +1,39 @@
+package com.simple.calculator.domain.service;
+
+import com.simple.calculator.domain.model.ExpressionModel;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorServiceTest {
+
+    @Test
+    void solveExpression() {
+        CalculatorService calculatorService = new CalculatorService();
+        ExpressionModel expressionModel = new ExpressionModel();
+        expressionModel.setExpression("1+1");
+        ExpressionModel expressionModelResult = new ExpressionModel();
+        expressionModelResult.setExpression("2.0");
+
+        calculatorService.buildExpression(expressionModel);
+        calculatorService.solveExpression();
+
+        assertEquals(
+                expressionModelResult.getExpression(),
+                calculatorService.getExpression().getExpression());
+    }
+
+    @Test
+    void buildExpression() {
+        CalculatorService calculatorService = new CalculatorService();
+        ExpressionModel expressionModel = new ExpressionModel();
+        expressionModel.setExpression("1+1");
+
+        calculatorService.buildExpression(expressionModel);
+
+        assertEquals(
+                expressionModel.getExpression(),
+                calculatorService.getExpression().getExpression());
+    }
+}
